6a9b854093de50cd5380bd38f1c7396cb53cc6b4
Nikita Skurenko <surmans51@gmail.com>
2024-08-16 15:06:34 +0600

    Third_Commit

--- src/Number.java
+++ src/Number.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Number {
     private int lengthMassiv;
 
-  private void lenghtMas() {
+  private void lengthMas() {
         Scanner scanner = new Scanner(System.in);
         do {
             System.out.print("Введите целое число от 1-ого до 10-ти:");
@@ -15,6 +15,34 @@ public class Number {
         } while (lengthMassiv <= 0 || lengthMassiv > 10);
   }
 
-
+    private char getMenu() {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Выберите номер операции:\n1 — режим игры" +
+              "\n2 — Ввод данных c файла или консоли" +
+              "\n3 — Изменение длинны исходного числа" +
+              "\n4 — Вкл./Выкл. повторяющиеся цифры");
+      int menuNumber = 0;
+      if (scanner.hasNextInt()) {
+          menuNumber = scanner.nextInt();
+      } else {
+          System.out.println("Введенно не корректное значение, " +
+                  "Введите повторно число, пожалуйста.");
+          return getMenu();
+      }
+      switch (menuNumber) {
+          case 1:
+              return '1';
+          case 2:
+              return '2';
+          case 3:
+              return '3';
+          case 4:
+              return '4';
+          default:
+              System.out.println("Неправильный пункт меню! " +
+                      "Пожалуйста, повторите операцию");
+      }
+      return scanner.next().charAt(0);
+    }
 
 }
